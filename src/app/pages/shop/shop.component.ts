0d9065489fc630939fbabb95f9b1fcd60df06c24
YuriiDmytryshyn <yuriidmytryshyn@gmail.com>
2020-12-24 17:16:17 +0200

    shop

--- src/app/pages/shop/shop.component.ts
+++ src/app/pages/shop/shop.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { IProduct } from 'src/app/shared/interfaces/product.interface';
 import { MenuService } from 'src/app/shared/services/menu.service';
+import { ProductsService } from 'src/app/shared/services/products.service';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-shop',
@@ -8,19 +11,35 @@ import { MenuService } from 'src/app/shared/services/menu.service';
 })
 export class ShopComponent implements OnInit {
 
+  products: Array<IProduct> = [];
+
   menuActive = 'translate3d(0,0,0)';
   menuStatus = false;
 
   constructor(
     private menuService: MenuService,
-  ) { }
+    private prodService: ProductsService,
+  ) {}
 
   ngOnInit(): void {
+    this.getProducts();
     this.menuService.menuStatus.subscribe((menuStatus) => {
       this.menuStatus = menuStatus;
       this.isMenuActive(this.menuStatus);
     });
   }
+
+  getProducts(): void {
+    this.prodService.getAll().snapshotChanges().pipe(
+      map(changes =>
+        changes.map(c =>
+          ({ id: c.payload.doc.id, ...c.payload.doc.data() })
+        )
+      )
+    ).subscribe(data => {
+      this.products = data;
+    });
+  };
 
   isMenuActive(status): void {
     if (status === false) {
